2f7821e26b4c6a7d8ee00635b1f4e0df125316ca
TuongLam <Tuonglamthien998@gmail.com>
2019-06-16 18:42:45 +0700

    initProject

---
+++ loop.js
@@ -0,0 +1,27 @@
+module.export = {
+  sandal : {
+    category: "Footwear",
+    price: 35,
+    bonus: {
+      type: "price",
+
+    }
+  },
+  sneaker : {
+    category: "Footwear",
+    price: 96,
+    bonus: {
+      type: "gift"
+    }
+  },
+  tshỉrt : {
+    category: "clothes",
+    price: 69,
+    bonus: true
+  },
+  candy: {
+    category: "foot",
+    price: 95,
+    bonus: false
+  }
+}
